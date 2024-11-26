cf2a8afb4be553fbbcc80f1740a83476f5b298f0
Caio Ribeiro <166857725+caiociribeiro@users.noreply.github.com>
2024-11-26 18:34:09 -0300

    modifica logica de normalização
    Normalização agora acontece dentro da hashtable e binarysearchtree, ao invés de antes de serem inseridas nestas

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.Normalizer;
 import java.util.Scanner;
 
 public class Main {
@@ -12,33 +11,13 @@ public class Main {
     public static String normalize(String s) {
         if (s == null || s.isEmpty()) return "";
 
-        // substitui letras acentuadas por sua letra base
-        s = Normalizer.normalize(s, Normalizer.Form.NFD);
-
-        // remove caracteres que nao sao ASCII
-        s = s.replaceAll("[^\\p{ASCII}]", "");
-
-        // remove numeros
-        s = s.replaceAll("[0-9]", "");
-
-        // remove hifen do comeco e final de palavras (palavras podem ter hifen no meio)
-        s = s.replaceAll("(^-|-$)", "");
-
-        // remove caracters especiais
-        s = s.replaceAll("[^a-zA-Z\\s-]", "");
-
-        // para palavras em ingles com apostrofo possessivo
-        s = s.replaceAll("(?i)['s]", "");
-
-        s = s.toLowerCase();
-
-        return s;
+        return s.replaceAll("^[^\\p{L}]+|[^\\p{L}]+$", "");
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        String keywordsPath = "C:\\Users\\caio\\unifor\\ed\\indice-remissivo\\src\\files\\chaves.txt";
-        String textPath = "C:\\Users\\caio\\unifor\\ed\\indice-remissivo\\src\\files\\texto.txt";
-        String indicePath = "C:\\Users\\caio\\unifor\\ed\\indice-remissivo\\src\\files\\indice.txt";
+        String keywordsPath = "C:\\Users\\alunok10\\Desktop\\indice-remissivo-main\\src\\files\\chaves.txt";
+        String textPath = "C:\\Users\\alunok10\\Desktop\\indice-remissivo-main\\src\\files\\texto.txt";
+        String indicePath = "C:\\Users\\alunok10\\Desktop\\indice-remissivo-main\\src\\files\\indice.txt";
 
         Scanner keywordsScanner = new Scanner(new File(keywordsPath));
 
@@ -49,9 +28,9 @@ public class Main {
 
             for (String s : line) {
                 s = normalize(s);
+
                 indice.put(s);
             }
-
         }
 
         Scanner textScanner = new Scanner(new File(textPath));
@@ -64,7 +43,7 @@ public class Main {
                s = normalize(s);
 
                if (!s.isEmpty()) {
-                   indice.update(s, currentLine);
+                  indice.update(s, currentLine);
                }
 
            }
